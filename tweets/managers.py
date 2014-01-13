d6c94a84e1e284f09e4b843a5ac6382167d4340c
jaymz <jaymz@u-dox.com>
2014-01-13 05:33:27 +0000

    set manager to not show the offical tweets by default

--- tweets/managers.py
+++ tweets/managers.py
@@ -4,4 +4,7 @@ from django.db import models
 class TweetManager(models.Manager):
 
     def get_queryset(self):
-        return super(TweetManager, self).get_queryset().exclude(deleted=True)
+        from models import MarketAccount
+
+        accounts = MarketAccount.objects.all().values_list('handle', flat=True)
+        return super(TweetManager, self).get_queryset().exclude(handle__in=accounts).exclude(deleted=True)
